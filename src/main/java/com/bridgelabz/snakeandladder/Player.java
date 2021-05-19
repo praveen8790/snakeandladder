9bd9e280e93bca8401b83381115b1a856936e595
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-19 20:11:23 +0530

    corrected the logical error

--- src/main/java/com/bridgelabz/snakeandladder/Player.java
+++ src/main/java/com/bridgelabz/snakeandladder/Player.java
@@ -5,19 +5,20 @@ import java.util.Random;
 public class Player {
 	public int player(int position) {
 		
-		Random random = new Random(); //instance of random class
+		Random random = new Random();
 		int int_random = random.nextInt(6) + 1;
 		int int_choice = random.nextInt(3) + 1;
 		if(int_choice == 2)	{
 			position = position + int_random;
+			if(position>100)
+				position=position-int_random;
 			player(position);
 		}
 		else if(int_choice == 3)
 			position = position - int_random;
 		if(position<0)
 			position=position-position;
-		if(position>100)
-			position=100;
+		
 		
 		return position;
 	}
